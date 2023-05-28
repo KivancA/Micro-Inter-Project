9c7f21464b086d8edc1ef5317d25003411301640
KivancA <55547181+KivancA@users.noreply.github.com>
2023-05-28 13:59:40 +0300

    Add files via upload
    Directly added all changes so far 2

---
+++ ArduinoCServer/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+   
+
+
+namespace ArduinoCServer
+{
+    public partial class Form1 : Form
+    {
+        private cServerClass Server;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string StringHost;
+            string StrIP;
+
+            try
+            {
+                StringHost = System.Net.Dns.GetHostName();
+                StrIP = Dns.GetHostByName(StringHost).AddressList[2].ToString();
+                label2.Text = "Host Name:" + StringHost + "IP Address:" + StrIP;
+            }
+            catch(Exception ex)
+            {
+
+            }
+
+            Server = new cServerClass();
+
+            try
+            {
+                richTextBox1.AppendText("-------Arduino Server Running-------");
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            Server.Message += RecInfo;
+
+        }
+
+        private delegate void UpdateTxtBox(RichTextBox Richbox1, string txt);
+
+        private void UpdateTxt(RichTextBox Richbox1, string txt)
+        {
+            if (InvokeRequired)
+                Richbox1.Invoke(new UpdateTxtBox(UpdateTxt), new object[] { richTextBox1, txt });
+            else if (txt != null)
+                Richbox1.AppendText(txt + "\n");
+
+        }
+        private void RecInfo(cServerClass sender, string data)
+        {
+            UpdateTxt(richTextBox1, data);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Server.IsLiserning = false;
+        }
+    }
+}
---
+++ ArduinoCServer/cServerClass.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace ArduinoCServer
+{
+    class cServerClass
+    {
+        public event MessageEventHandler Message;
+
+        public delegate void MessageEventHandler(cServerClass sender, string Data);
+
+        //Server Control
+        public IPAddress ServerIP = IPAddress.Parse("192.168.0.14");
+        public int ServerPort = 1000;
+        public TcpListener myserver;
+
+        public Thread Comthread;
+        public bool IsLiserning = true;
+
+        //Clients
+        private TcpClient client;
+        private StreamReader clientdata;
+
+        public cServerClass()
+        {
+            myserver = new TcpListener(ServerIP, ServerPort);
+            myserver.Start();
+
+            Comthread = new Thread(new ThreadStart(Hearing));
+            Comthread.Start();
+
+        }
+
+        private void Hearing()
+        {
+            while (!IsLiserning == false)
+            {
+                if (myserver.Pending() ==true)
+                {
+                    client = myserver.AcceptTcpClient();
+                    clientdata = new StreamReader(client.GetStream());
+                }
+
+                try
+                {
+                    Message?.Invoke(this, clientdata.ReadLine());
+                }
+                catch (Exception ex)
+                {
+
+                }
+                Thread.Sleep(10);
+
+            }
+        }
+
+    }
+}
